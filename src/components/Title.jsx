18dc559520f504604a25029d4ae6fba2f36c7b2a
muwongelawrence-tech <muwongelawrence44@gmail.com>
2023-10-04 12:41:58 +0300

    Adding animation to the title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,24 +2,26 @@ import React, { useEffect, useState } from "react";
 
 function Title({ title, className }) {
 
-  const [animationClass, setAnimationClass] = useState('');
+  const [animate, setAnimate] = useState(false);
 
   useEffect(() => {
-    // Apply the animation class after a short delay to ensure it triggers on load
-    const timer = setTimeout(() => {
-      setAnimationClass('animate-slide-up');
-    }, 100);
-
-    return () => clearTimeout(timer);
+      setTimeout(() => {
+        setAnimate(true);
+      }, 100);
   }, []);
+
   return (
-    <div className="w-full flex items-center justify-center">
+    <div
+      className={`w-full flex items-center justify-center ${
+        animate ? "animate" : ""
+      }`}
+    >
       <p
         className={`text-2xl md:text-5xl font-bold max-w-xs md:max-w-2xl text-center ${
           className ? className : "text-[#161616]"
-        } ${animationClass} `}
+        } transform translate-y-4 transition-transform duration-1000 ease-in-out`}
       >
-        {title}
+        { title } 
       </p>
     </div>
   );
