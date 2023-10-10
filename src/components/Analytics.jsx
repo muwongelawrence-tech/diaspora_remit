31c128637978dfa4b8955cf6c7bbc51a2e649065
muwongelawrence-tech <muwongelawrence44@gmail.com>
2023-10-10 13:55:53 +0300

    adding react count up to the analytics

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -5,29 +5,33 @@ function Analytics() {
   const analyticsData = [
     {
       id: 1,
-      value: "10+",
+      value: 10,
+      extension: "+",
       description: "Years experience",
     },
     {
       id: 2,
-      value: "15k+",
+      value: 15,
+      extension: "k+",
       description: "Positive Review",
     },
     {
       id: 3,
-      value: "10k",
+      value: 10,
+      extension: "k",
       description: "Global downloads",
     },
     {
       id: 4,
-      value: "99%",
+      value: 99,
+      extension: "%",
       description: "Return on investment",
     },
   ];
   return (
     <div className="w-full py-2 px-3 grid grid-cols-2 lg:grid-cols-4 gap-4 items-center justify-center my-10">
-      { analyticsData.map(({ id, value, description }) => (
-        <AnalyticsCard key={id} value={value} description={description} />
+      { analyticsData.map(({ id, value, description, extension }) => (
+        <AnalyticsCard key={id} value={value} description={description} extension = {extension} />
       ))}
     </div>
   );
--- src/components/AnalyticsCard.jsx
+++ src/components/AnalyticsCard.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 
-function AnalyticsCard({ value, description }) {
+function AnalyticsCard({ value, description, extension }) {
+  const [counterOn, setCounterOn] = useState();
+  const [ countupTo , setCountupTo] = useState(200);
+;
+
   return (
     <div className="w-full flex flex-col items-center justify-center">
-      <p className="text-2xl md:text-5xl font-bold max-w-xs md:max-w-2xl text-left text-[#161616]">
-        {value}
-      </p>
-      <p className="text-left text-[#161616]">{description}</p>
+      <ScrollTrigger
+        onEnter={() => setCounterOn(true)}
+        onExit={() => setCounterOn(false)}
+      >
+        <div className="flex flex-col items-center justify-center">
+          <p className="text-2xl md:text-5xl font-bold max-w-xs md:max-w-2xl text-left text-[#161616]">
+            {counterOn && (
+              <CountUp start={0} end={countupTo} delay={0} duration={2} />
+            )}
+            <span>{extension}</span>
+          </p>
+          <p className="text-left text-[#161616]">{description}</p>
+        </div>
+        </ScrollTrigger>
     </div>
   );
 }
